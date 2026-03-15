f533e90fd1c46b219f8eb3633cbb5eea98e1e68a
Dennis Ampat <theluftwaffe@outlook.com>
2026-03-15 22:23:30 +0800

    feat: update portfolio layout with new sections, enhanced styling, and responsive design

---
+++ src/components/NeonCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="relative backdrop-blur-md bg-gray-950/5 rounded-lg min-h-[280px]"
+    :class="custom_class"
+  >
+    <!-- border overlays -->
+    <div class="absolute inset-0 rounded-lg border border-3
+      border-t-neon-pink border-l-neon-pink
+      border-b-transparent border-r-transparent opacity-25 -z-10">
+    </div>
+    <div class="absolute inset-0 rounded-lg border border-3
+      border-b-neon-purple border-r-neon-purple
+      border-t-transparent border-l-transparent opacity-10 -z-10">
+    </div>
+
+    <!-- slot -->
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  custom_class: {
+    required: false,
+    type: String,
+    default: ''
+  }
+})
+</script>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,16 +1,87 @@
 <template>
-  <section id="contact" class="max-w-6xl mx-auto px-4 py-16">
+  <section id="contact" class="max-w-6xl mx-auto px-4 py-16 min-h-[80vh] 2560:min-h-[50vh] scroll-mt-20">
     <div class="flex gap-3">
-      <span class="font-playfair tracking-[8px] text-2xl font-bold text-[var(--neon-cyan)]">//</span>
-      <span class="text-2xl font-semibold text-[var(--neon-pink)] tracking-widest">Contact</span>
+      <span class="font-playfair tracking-[8px] text-2xl font-bold text-neon-cyan">//</span>
+      <span class="text-2xl font-semibold text-neon-pink tracking-widest">Contact</span>
     </div>
 
-    <p class="text-gray-400 mb-6">
-      Interested in working together or hiring.
-    </p>
+    <div class="mt-6">
+      <p class="text-xl md:text-4xl text-gray-200 font-bold tracking-wider mb-6 text-gray-300">
+        Let's work together
+      </p>
+      <div class="flex gap-2 items-center">
+        <bullet></bullet>
 
-    <button class="bg-indigo-600 px-6 py-3 rounded-lg hover:bg-indigo-500">
-      Email Me
-    </button>
+        <span class="text-sm md:text-2xl text-gray-300">
+          Open for backend roles and freelance work.
+        </span>
+      </div>
+    </div>
+    <div class="flex flex-wrap justify-center gap-4 mt-6">
+      <neon-card :class="'flex-1'">
+        <div class="flex flex-col justify-evenly p-6 h-full text-center">
+          <div class="relative w-12 h-12 mx-auto">
+            <img src="@/assets/images/logos/icons8-email-50.png" alt="email" class="w-full h-full">
+            <div class="absolute inset-0 bg-neon-purple/20 mix-blend-color"></div>
+            <div class="absolute inset-0 bg-gradient-to-t from-gray-950 via-gray-950/40 to-transparent"></div>
+          </div>
+
+          <span class="text-neon-cyan text-xl">dennis.ampat@gmail.com</span>
+
+          <a
+            href="mailto:dennis.ampat@gmail.com"
+            class="bg-neon-green py-2 rounded-md text-lg text-gray-900 font-semibold tracking-wide"
+          >
+            Email Me
+          </a>
+        </div>
+      </neon-card>
+      <neon-card :class="'flex-1'">
+        <div class="flex flex-col justify-evenly p-6 h-full text-center">
+          <div class="relative w-12 h-12 mx-auto">
+            <img src="@/assets/images/logos/icons8-github-48.png" alt="github" class="w-12 h-12 mx-auto">
+            <div class="absolute inset-0 bg-neon-purple/20 mix-blend-color rounded-full"></div>
+            <div
+              class="absolute inset-0 bg-gradient-to-t from-gray-950 via-gray-950/40 to-transparent rounded-full"
+            ></div>
+          </div>
+
+          <span class="text-neon-cyan text-xl">github.com/Resaizu</span>
+
+          <a
+            href="https://github.com/Resaizu"
+            class="bg-neon-green py-2 rounded-md text-lg text-gray-900 font-semibold tracking-wide"
+          >
+            View
+          </a>
+        </div>
+      </neon-card>
+      <neon-card :class="'flex-1'">
+        <div class="flex flex-col justify-evenly p-6 h-full text-center">
+          <div class="relative w-12 h-12 mx-auto">
+            <img src="@/assets/images/logos/icons8-linkedin-50.png" alt="linkedin" class="w-12 h-12 mx-auto">
+            <div class="absolute inset-0 bg-neon-purple/20 mix-blend-color rounded-md"></div>
+            <div
+              class="absolute inset-0 bg-gradient-to-t from-gray-950 via-gray-950/40 to-transparent rounded-md"
+            ></div>
+          </div>
+
+          <span class="text-neon-cyan text-xl">linkedin.com/in/dennis-v-ampat</span>
+
+          <a
+            href="https://linkedin.com/in/dennis-v-ampat"
+            class="bg-neon-green py-2 rounded-md text-lg text-gray-900 font-semibold tracking-wide"
+          >
+            View
+          </a>
+        </div>
+      </neon-card>
+    </div>
+    <div class="neon-line mt-12"></div>
   </section>
 </template>
+
+<script setup>
+import NeonCard from '@/components/NeonCard.vue';
+import Bullet from '@/components/Bullet.vue';
+</script>
